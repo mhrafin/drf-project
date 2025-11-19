2610a2db7d79a5491459658577cff202aa03aeca
rafwork <110781737+mhrafin@users.noreply.github.com>
2025-11-19 17:04:05 +0600

    Modelserializers

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -3,18 +3,37 @@ from rest_framework import serializers
 from watchlist_app.models import Movie
 
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
-    description = serializers.CharField()
-    active = serializers.BooleanField()
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        # fields = "__all__"
+        exclude = ["active"]
 
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError("Title and Description should be different!")
+        else:
+            return data
+        
+    def validate_name(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError("Name is too short!")
+        else:
+            return value
 
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get("name", instance.name)
-        instance.description = validated_data.get("description", instance.description)
-        instance.active = validated_data.get("active", instance.active)
-        instance.save()
-        return instance
+
+# class MovieSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     name = serializers.CharField()
+#     description = serializers.CharField()
+#     active = serializers.BooleanField()
+
+#     def create(self, validated_data):
+#         return Movie.objects.create(**validated_data)
+
+#     def update(self, instance, validated_data):
+#         instance.name = validated_data.get("name", instance.name)
+#         instance.description = validated_data.get("description", instance.description)
+#         instance.active = validated_data.get("active", instance.active)
+#         instance.save()
+#         return instance
