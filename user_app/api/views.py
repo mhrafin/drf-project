86e367ce7e6e7148d13135fb33d84eb816c06260
rafhome <110781737+mhrafin@users.noreply.github.com>
2025-12-03 22:50:45 +0600

    Auto-save: Wed  3 Dec 22:50:45 +06 2025

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -2,6 +2,9 @@ from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
+from rest_framework import status
+
+from django.core.exceptions import ObjectDoesNotExist
 
 from user_app.api.serializers import RegistrationSerializer
 
@@ -22,4 +25,22 @@ class RegistrationAPIView(APIView):
         else:
             return Response(serializer.errors)
         
-    
+
+class LogoutAPIView(APIView):
+
+    def post(self, request):
+        return self.logout(request)
+
+    def logout(self, request):
+        try:
+            request.user.auth_token.delete()
+        except (AttributeError, ObjectDoesNotExist):
+            pass
+
+        # logout(request)
+
+        return Response({"success": "Successfully logged out."},
+                        status=status.HTTP_200_OK)
+
+
+
